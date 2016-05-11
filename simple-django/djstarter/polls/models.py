5f27284c1358ba0e26fa486755d79409baebfcf1
Thang Nguyen <thang.nguyenphuoc@asnet.com.vn>
2016-05-11 15:53:01 +0700

    Update vote, results for polls

--- simple-django/djstarter/polls/models.py
+++ simple-django/djstarter/polls/models.py
@@ -1,3 +1,4 @@
+"""Polls."""
 import datetime
 
 from django.db import models
@@ -5,20 +6,27 @@ from django.utils import timezone
 
 
 class Question(models.Model):
+    """Define attribute for Question."""
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-            return self.question_text
+        """Question text."""
+        return self.question_text
 
 
 class Choice(models.Model):
+    """Define attribute for Choice."""
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-            return self.choice_text
+        """Choice_text."""
+        return self.choice_text
 
     def was_published_recently(self):
-            return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        """Published recently."""
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
