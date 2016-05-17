ba67527417eda5ecdc5f9f281c4be55acaace0ef
Thang Nguyen <thang.nguyenphuoc@asnet.com.vn>
2016-05-17 17:09:45 +0700

    Update generic view for blogs

--- simple-django/djstarter/blog/views.py
+++ simple-django/djstarter/blog/views.py
@@ -1,53 +1,62 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.generic import ListView
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
-def post_list(request):
+# def post_list(request):
 
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)  # 3 posts in each page
-    page = request.GET.get('page')
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 3)  # 3 posts in each page
+#     page = request.GET.get('page')
 
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         # If page is not an integer deliver the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page is out of range deliver last page of results
+#         posts = paginator.page(paginator.num_pages)
 
-    return render(
-        request,
-        'blog/post/list.html',
-        {
-            'posts': posts,
-            'page': page,
-        }
-    )
+#     return render(
+#         request,
+#         'blog/post/list.html',
+#         {
+#             'posts': posts,
+#             'page': page,
+#         }
+#     )
 
 
-def post_detail(request, year, month, day, post):
+# def post_detail(request, year, month, day, post):
 
-    post = get_object_or_404(
-        Post,
-        slug=post,
-        status='published',
-        publish__year=year,
-        publish__month=month,
-        publish__day=day
-    )
+#     post = get_object_or_404(
+#         Post,
+#         slug=post,
+#         status='published',
+#         publish__year=year,
+#         publish__month=month,
+#         publish__day=day
+#     )
 
-    return render(
-        request,
-        'blog/post/detail.html',
-        {'post': post}
-    )
+#     return render(
+#         request,
+#         'blog/post/detail.html',
+#         {'post': post}
+#     )
 
-# class PostListView(ListView):
-#     queryset = Post.published.all()
-#     context_object_name = 'posts'
-#     paginate_by = 3
-#     template_name = 'blog/post/list.html'
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+class PostDetailView(DetailView):
+    # queryset = Post.published.all()
+    # context_object_name = 'posts'
+    # paginate_by = 3
+    model = Post
+    template_name = 'blog/post/detail.html'
