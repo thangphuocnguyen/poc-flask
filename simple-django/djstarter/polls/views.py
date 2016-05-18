070cae77d90df517b22957ba2eefb1651fac430f
Thang Nguyen <thang.nguyenphuoc@asnet.com.vn>
2016-05-18 15:40:46 +0700

    Update F function for choose

--- simple-django/djstarter/polls/views.py
+++ simple-django/djstarter/polls/views.py
@@ -4,11 +4,14 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.utils import timezone
 from django.views import generic
+from django.db.models import F
 
 from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
+    """Index View for Poll."""
+
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
@@ -18,22 +21,27 @@ class IndexView(generic.ListView):
 
 
 class DetailView(generic.DetailView):
+    """Detail View for Poll."""
+
     model = Question
     template_name = 'polls/detail.html'
 
     def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
+        """Exclude any questions that aren't published yet."""
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
+    """Result view for Poll."""
+
     model = Question
     template_name = 'polls/results.html'
 
+
 def vote(request, question_id):
+    """Vote."""
     question = get_object_or_404(Question, pk=question_id)
+
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
@@ -43,17 +51,21 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
+        selected_choice.votes += F('votes') + 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse(
+            'polls:results',
+            args=(question.id,)
+        ))
+
 
 def get_queryset(self):
-    """
-    Return the last five published questions (not including those set to be
-    published in the future).
+    """Return the last five published questions.
+
+    (not including those set to be published in the future).
     """
     return Question.objects.filter(
         pub_date__lte=timezone.now()
