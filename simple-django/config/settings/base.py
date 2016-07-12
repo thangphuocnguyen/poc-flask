61384ed6d85ad0dfd08e495885818422113eed3f
Thang Nguyen <thang.nguyenphuoc@asnet.com.vn>
2016-07-12 17:59:34 +0700

    add for prepare example with gunicorn static file

--- simple-django/config/settings/base.py
+++ simple-django/config/settings/base.py
@@ -128,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "sfiles"), )
 
 # ////////// CONFIGURATION TASTYPIE API
 TASTYPIE_DEFAULT_FORMATS = ['json']
